25ccebecadabc35c9b1971fba2a29cd6285eb802
tailsUp <sorsa.git@gmail.com>
2024-10-02 19:25:33 +0300

    dist sisään

--- frontend/src/services/loginService.js
+++ frontend/src/services/loginService.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/api/login'
+const baseUrl2 = 'https://cicd-repo.onrender.com/:3001/api/login'
 
 const login = async credentials => {
-  const response = await axios.post(baseUrl, credentials).catch(error => {
+  console.log('loginService: ', credentials)
+  const response = await axios.post(baseUrl2, credentials).catch(error => {
     console.log('Error in logging in. ' ,error)
   })
   return response.data
