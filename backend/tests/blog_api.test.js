1d9ee8a1514102c371c533c85d8526e43e2c2d3f
tailsUp <sorsa.git@gmail.com>
2024-10-09 13:53:08 +0300

    kokeillaan normi testit nyt kun secretit asetettu. 7

--- backend/tests/blog_api.test.js
+++ backend/tests/blog_api.test.js
@@ -12,7 +12,7 @@ let ROOT = ''
  */
 describe('ALL REQUIRED BLOG TESTS FOR WEEK 4', () => {
     beforeEach(async () => {
-        jest.useFakeTimers('legacy')
+        jest.useFakeTimers({ advanceTimers: true });
         await Blogs.deleteMany({})                                                  //Poistetaan vanhat
         ROOT = await Users.collection.findOne({"username": 'root'})                 //Haetaan rootkäyttäjä
         const testBlogs = helper.setUserIdForBlogs(ROOT._id, helper.initialBlogs)   //Asetetaan root_id blogeille.
